ccdea5f9487e2c7dea08910393478736e3ab8289
Ankita <125357910+Ankitatiwary21@users.noreply.github.com>
2026-08-31 01:25:05 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        
+        int step1 = 1;
+        int step2 = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            int current_step = step1 + step2;
+            step1 = step2;
+            step2 = current_step;
+        }
+        
+        return step2;
+    }
+}
